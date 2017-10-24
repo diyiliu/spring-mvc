0de6eab20dd737215f68f3edccdc4fb2e7828796
DIYILIU <572772828@qq.com>
2017-10-24 16:50:01 +0800

    测试成功

--- src/main/java/com/diyiliu/controller/HomeController.java
+++ src/main/java/com/diyiliu/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.diyiliu.controller;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Description: HomeController
@@ -15,18 +18,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index(@RequestParam(defaultValue = "world") String name, Model model){
         model.addAttribute("message", "你好," + name);
 
-        return "index";
+        return "indexPage";
     }
 
-    @RequestMapping("/search")
-    public String search(){
+    @RequestMapping("/")
+    public String home(){
 
-        new LayoutDialect();
-
-        return "search";
+        return "searchPage";
     }
+
+    @RequestMapping("/result")
+    public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
+        model.addAttribute("search", search);
+
+        return "resultPage";
+    }
+
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String search(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+
+        if (search.toLowerCase().equals("struts")){
+
+            redirectAttributes.addFlashAttribute("error", "struts 已过时, 请使用spring!");
+            return "redirect:/";
+        }
+
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
+    }
+
 }
